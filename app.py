3f213d0970e783bfdc87221ef549787630e28d97
Mugema Francis Ryan <54482161+Mugema@users.noreply.github.com>
2025-11-17 02:02:24 +0300

    Fix launch parameters for demo server

--- app.py
+++ app.py
@@ -101,4 +101,4 @@ with gr.Blocks() as demo:
 port = int(os.environ.get("PORT", 7860))
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=port)
+    demo.launch(server.port ="10000",server.address=" 0.0.0.0")
