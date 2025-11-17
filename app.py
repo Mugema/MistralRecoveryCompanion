61f2c242f01088c25e1b0f056259d993dea42033
Mugema Francis Ryan <54482161+Mugema@users.noreply.github.com>
2025-11-17 04:54:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,9 @@ with gr.Blocks() as demo:
     chatbot.render()
 
 
-port = int(os.environ.get("PORT", 7860))
-
 if __name__ == "__main__":
-    demo.launch()
+    PORT = int(os.environ.get("PORT", 8080))
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=PORT
+    )
